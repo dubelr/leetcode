4912243a76cc7a10f1caa36769d455ce97b0c8fb
Bobby Dubel <bobby@plainspokendigital.com>
2022-11-17 08:09:51 -0500

    NONFUNCTIONING, first commit

--- Java/0021_MergeTwoSortedLists.java
+++ Java/0021_MergeTwoSortedLists.java
@@ -8,11 +8,8 @@
 * in a one sorted list. The list should be made by splicing together the nodes of the first two lists. 
 * Return the head of the merged linked list.
 * 
-* Runtime range: 1ms (81.82%)
-* Space range: 42-43MB (8%)
-* 
-* Time to submission: 25mins
-* Time to correct: 
+* Runtime: 1ms (81.82%)
+* Space: 42-43MB (8%)
 */
 
 /*
---
+++ script/README_main.md
@@ -0,0 +1,3 @@
+# Solutions for LeetCode
+![](https://img.shields.io/badge/language-Python-blue)
+![](https://img.shields.io/badge/%3E-leetcode-yellow)
