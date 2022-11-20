41655d1cd7f3dc65030a9db82c0be47fb6fa5a45
Bobby Dubel <bobby@plainspokendigital.com>
2022-11-20 12:11:21 -0500

    new solutions

--- Java/0021_MergeTwoSortedLists.java
+++ Java/0021_MergeTwoSortedLists.java
@@ -3,7 +3,7 @@
 * Title:  Merge Two Sorted Lists
 * Link: https://leetcode.com/problems/merge-two-sorted-lists/
 * Tags: 
-* Level: 
+* Difficulty: 
 * Content: You are given the heads of two sorted linked lists list1 and list2. Merge the two lists 
 * in a one sorted list. The list should be made by splicing together the nodes of the first two lists. 
 * Return the head of the merged linked list.
