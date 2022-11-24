e0db0fcd03adb9a81c522bcb3e2bb1b9e3c2bed8
Bobby Dubel <bobby@plainspokendigital.com>
2022-11-24 10:25:37 -0500

    new solution

---
+++ Java/0110_BalancedBinaryTree.java
@@ -0,0 +1,54 @@
+/**
+ * Number: 110
+ * Title: Balanced Binary Tree
+ * Link: https://leetcode.com/problems/balanced-binary-tree/
+ * Tags: Tree, Depth-First Search, Binary Tree
+ * Difficulty: Easy
+ * Content: Given a binary tree, determine if it is height-balanced.
+ * 
+ * Original runtime: N/A (unsolved)
+ * Original space: N/A (unsolved)
+ * 
+ * Improved runtime: 1ms (99.17%)
+ * Improved space: 43.9MB (72.29%)
+ */
+
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        return depthBalanced(root) != -1;
+    }
+    
+    public int depthBalanced(TreeNode root){
+        // trivially balanced and height 0 if root == null
+        if(root == null) {
+            return 0;
+        }
+        // get height of left and right
+        int heightLeft = depthBalanced(root.left);
+        if(heightLeft == -1){
+            return -1;
+        }
+        int heightRight = depthBalanced(root.right);
+        if(heightRight == -1) {
+            return -1;
+        }
+        if(Math.abs(heightLeft - heightRight) > 1) {
+            return -1;
+        }
+        return 1 + Math.max(heightLeft, heightRight);                
+    }
+}
+
+//Definition for a binary tree node.
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+      this.val = val;
+      this.left = left;
+      this.right = right;
+    }
+  }
--- Java/0242_ValidAnagram.java
+++ Java/0242_ValidAnagram.java
@@ -10,30 +10,12 @@ import java.util.Arrays;
  * false otherwise.An Anagram is a word or phrase formed by rearranging the
  * letters of a different word or phrase, typically using all the original
  * letters exactly once.
- * Runtime: 11ms (58.49%)
- * Space: 4575MB (24.22%)
+ * 
+ * Runtime Original: 11ms (58.49%)
+ * Space Original: 4575MB (24.22%)
  */
 
 class Solution {
-    public boolean isAnagram(String s, String t) {
-        if (s.length() != t.length()) {
-            return false;
-        }
-        char[] sArr = s.toCharArray();
-        char[] tArr = t.toCharArray();
-        Arrays.sort(sArr);
-        Arrays.sort(tArr);
-        for (int i = 0; i < sArr.length; ++i) {
-            if (sArr[i] != tArr[i]) {
-                return false;
-            }
-        }
-        return true;
-
-    }
-}
-
-class SolutionImproved {
     public boolean isAnagram(String s, String t) {
         int[] a = new int[26];
         for (char c : s.toCharArray()) {
@@ -49,3 +31,21 @@ class SolutionImproved {
         return true;
     }
 }
+
+// class SolutionOriginal {
+//     public boolean isAnagram(String s, String t) {
+//         if (s.length() != t.length()) {
+//             return false;
+//         }
+//         char[] sArr = s.toCharArray();
+//         char[] tArr = t.toCharArray();
+//         Arrays.sort(sArr);
+//         Arrays.sort(tArr);
+//         for (int i = 0; i < sArr.length; ++i) {
+//             if (sArr[i] != tArr[i]) {
+//                 return false;
+//             }
+//         }
+//         return true;
+//     }
+// }
